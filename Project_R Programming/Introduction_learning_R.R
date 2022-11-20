c914b5a99dba2717db89e8753988c8572e46cbde
Sprasaming <45584130+sprasaming@users.noreply.github.com>
2022-11-20 19:49:06 +0700

    Update R Programming Foundation
    Update R Programming Foundation

--- Project_R Programming/Introduction_learning_R.R
+++ Project_R Programming/Introduction_learning_R.R
@@ -1,315 +1,353 @@
-## create new variables
+#### R Foundation #
+### Variables ####
 
-income <- 6000
-expense <- 3200
+# assign
+income <- 45000
+expense <- 26000
 saving <- income - expense
 
-## remove variables
-
+# remove
+rm(income)
+rm(expense)
 rm(saving)
 
-#### Print characters (string)
 
-## print hello world
-
-print ("hello world")
-print ('hello world')
-
-## there are other function too
-
-cat("R is awesome!")
-
-## define your own function
-
-my_sum <- function(num1, num2=10){
-  num1 + num2
-}
-
-## test function
-my_sum(20,0)
-
-
-## call function in library
-library(glue)
-
-my_names <- "Nueng"
-my_ages <- 31
-
-glue("Hello my name is {my_names}. This year I'm {my_ages} years old")
-
-
-##hello()
-
-hello <- function (user_name){
-  glue("Hello! {user_name}")
-}
-
-
-#### data type
-
-#numric
+### Data types ####
+## numeric
 class(100)
 
-#character
+## character
 class("Hello World")
 
-#logical
-class(TRUE) #FALSE
+## logical
+class(TRUE)
 
-#### data type
-my_ages <- 31
-class(my_ages)
+## factor
+# nominal factor
+animal <- c("cat", "dog", "hippo", "cat", "dog")
+animal <- as.factor(animal)
+class(animal)
+# ordinal factor(sort)
+performance <-
+    c("Poor", "Average", "Good", "Good", "Poor", "Average", "Good")
+performance <- factor(performance,
+                      levels = c("Poor", "Average", "Good"),
+                      ordered = TRUE)
 
-fav_book <- "Business made simple"
-class(fav_book)
 
-movie_lover <- TRUE
-class(movie_lover)
+### Data structure ####
+## Vector
+friends <- c("toy", "ink", "aan", "top", "wan")
+length(friends)
 
-puppy <- c("cat", "dog", "dog", "cat", "cat")
-class (puppy)
+# subset by position
+friends[1]
+friends[2]
+friends[3]
 
-puppy <- as.factor(puppy)
+friends[1:3]
+friends[4:5]
 
-today <- as.Date("2021-09-04")
+friends[c(1, 3, 5)]
+friends[c(5, 2, 4)]
 
-## data structures
-## vector, matrix, list, dataframe
+## Matrix
+matrix(1:25, ncol = 5)
+m <- matrix(1:10, ncol = 2, byrow = TRUE)
 
-# 1. vector
-1:10
-89:205
+nrow(m)
+ncol(m)
 
-seq(from=1, to=100, by=4)
+m * 2
 
-my_friends <- c ("Toy", "Aan", "YOS", "Ink", "Top")
-ages <- c (33, 33, 30, 29, 28)
-movie_lover <- c (T, F, T, F, T)
+m1 <- matrix(c(1,5,3,6),ncol=2)
+m2 <- matrix(c(2,4,5,5),ncol=2)
 
-# 2. matrix
-m1 <- matrix(1:25,ncol = 5 , byrow = T)
+# element-wise computation
+m1 + m2
+m1 * m2
 
-ncol(m1)
-nrow(m1)
-dim(m1)
+# matrix multiplication .dot notation
+m1 %*% m2
 
-# 3. list
-customer_data <- list(
-  id = 1,
-  name = "David",
-  location = "London",
-  language = "English"
+
+## Dataframe ####
+friends <- c("toy", "ink", "aan", "top", "wan")
+ages <- c(33, 30, 34, 28, 25)
+movie_lover <- c(TRUE, TRUE, FALSE, T, F)
+jobs <- as.factor(c("data", "data", "digital", "digital", "phd"))
+handsome_degree <- factor(
+    c("low", "medium", "high", "high", "high"),
+    levels = c("low", "medium", "high"),
+    ordered = TRUE
 )
 
-customer_data$name
-customer_data[[2]]
+friends_df <- data.frame(friend_id = 1:5,
+                         friends,
+                         ages,
+                         movie_lover,
+                         jobs,
+                         handsome_degree)
+class(df)
+
+# Write csv file
+write.csv(df, "friends.csv",
+          row.names = FALSE)
+
+# Read csv file
+df <- read.csv("friends.csv")
+
+# Read csv from internet
+url <-
+    "https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv"
+
+mtcars_df <- read.csv(url)
+
+# Dataframe subset
+friends_df[1:3, ] # row 1:3, all column
+friends_df[4:5, ] # row 4:5, all column
+
+friends_df[1:3, 1:3] # row 1:3, column 1:3
+friends_df[1:3, 4:5] # row 1:3, column 4:5
+
+friends_df[, c(2, 4, 5)] # all row, column 2,4,5
+
+friends_df[friends_df$ages < 30] # row age < 30, all column
+friends_df[friends_df$ages < 30, c(2, 4, 5)] # row age < 30, column 2,4,5
+
+friends_df[friends_df$movie_lover, ] # movie_lover true, all column
+
+friends_df[friends_df$jobs != "digital", ] # job not digital, all column
+
+friends_df[friends_df$job %in% c("data", "digital"),]
+
+friends_df[, c("friends", "ages", "movie_lover")]
+
+cond1 <- friends_df$job == "data"
+cond2 <- friends_df$movie_lover
+friends_df[cond1 & cond2,]
+friends_df[cond1 | cond2,]
+
+# Aggregate
+ages <- friends_df$ages
+mean(ages)
+sum(ages)
+median(ages)
+min(ages)
+max(ages)
+sd(ages)
+var(ages)
+
+# create new column
+friends_df$segment <- ifelse(friends_df$ages >= 30, "Old", "Young")
+
+table(friends_df$segment) # frequency table
+table(friends_df$job)
+
+ncol(friends_df)
+nrow(friends_df)
+
+# proportion
+table(friends_df$segment) / nrow(friends_df)
+
+summary(friends_df)
+str(friends_df)
+
+friends_df$jobs <- factor(friends_df$jobs)
+friends_df$segment <- factor(friends_df$segment)
+
+summary(friends_df)
+str(friends_df)
 
 
-customer_data <- list(
-  customer_01 = list(
-    name = "David",
-    location = "London",
-    manchester_fan = TRUE
-  ),
-  customer_02 = list(
-    name = "Kego",
-    location = "Tokyo"
-  ),
-  customer_03 = list(
-    name = "Anna",
-    location = "Washington",
-    movie_lover = TRUE,
-    visit_bank = TRUE,
-    account_blance = 20000
-  )  
+# drop column
+friends_subset <- friends_df[, 1:5]
+
+friends_df$handsome_degree <- NULL
+
+str(friends_df)
+
+
+## List
+john <- list(
+    fullname = "John Davis",
+    age = 25,
+    city = "London",
+    email = "john.davis@gmail.com"
+)
+marry <- list(
+    fullname = "Marry Anne",
+    age = 22,
+    city = "Liverpool",
+    email = "marry.anne@gmail.com"
+)
+kevin <- list(
+    fullname = "Kevin Herst",
+    age = 25,
+    city = "USA",
+    email = "kevin.h@gmail.com",
+    movies = c("Batman","Dark Knight")
 )
 
-customer_data[[3]][[4]]
+# nested list
+customers <- list(john=john,marry=marry,kevin=kevin)
+customers
+customers$john
+customers$john$fullname
+customers$john$age
+
+customers[["kevin"]]
+customers[["kevin"]][["email"]]
 
 
-## DataFrame
-my_friends <- c ("Toy", "Aan", "YOS", "Ink", "Top")
-ages <- c (33, 33, 30, 29, 28)
-movie_lover <- c (T, F, T, F, T)
+### Control Flow ####
+## if, for, while
 
-df <- data.frame(id = 1:5, 
-                 my_friends, 
-                 ages, 
-                 movie_lover)
-
-View(df)
-
-## write .csv file
-write.csv(df,"friend.csv", row.names = FALSE)
-
-
-## read .csv file
-
-friend <- read.csv("friend.csv")
-
-class(friend)
-
-# subset dataframe
-# subset = extract data
-friend[1:2, 2:3]
-friend[1:4, 1:4]
-friend[1:4, ]
-friend[ c(1,3,4), ]
-friend[ , "my_friends"]
-friend[ , c("my_friends", "ages")]
-
-friend$ages < 30
-
-friend[ friend$ages >= 30, ]
-friend [ c(F,F,F,T,T), ]
-
-friend[ friend$movie_lover , ]
-friend[ !friend$movie_lover , ]
-
-new_friend <- data.frame(
-  id = 6:7,
-  my_friends = c("Ball", "Wann"),
-  ages = c(32, 26),
-  movie_lover = c(T, T)
-)
-
-# row bind
-all_friends <- rbind(friend, new_friend)
-
-
-# add column
-all_friends$correct_age <- all_friends$ages - 1
-
-# remove Column
-all_friends$ages <- NULL
-
-# rename column  #R start index 1 ,Python start index 0   
-names(all_friends)[2] <- "name"
-
-mini_friends <- all_friends[ -c(3,4), ]
-
-## Control Flow
-
-## 1. if
-
-score <- 55
-
+## if
+score <- 85
 if (score >= 80) {
-  print("Passed")
+    print("Passed")
 } else {
-  print("Failed")
-}
-
-
-
-check_grade <- function(score){
-  if (score >= 80) {
-    print("A")
-  } else {
     print("Failed")
-  }
 }
 
-score <-90
-ifelse(score >=80, "Passed","Failed")
-
-check_grade_2 <- function(score){
-  ifelse(score >=80, "Passed","Failed")
-}
-
-##  if
-## A,B,C,D,F
-
-check_grade <- function(score){
-  if (score >= 80) {
+if (score >= 90) {
     print("A")
-  } else if(score >= 70) {
+} else if (score >= 80) {
     print("B")
-  } else if(score >= 60) {
-    print("C") 
-  } else if(score >= 50) {
+} else if (score >= 70) {
+    print("C")
+} else{
     print("D")
-  } else {
-    print("F")
-  }
 }
 
-## 2. for loop
+# ifelse vectorization in r
+ifelse(scores >= 80, "Passed", "Failed")
 
-numbers <- c(10, 15, 20, 50)
 
-for (num in numbers){
-  print (num*2)
+## for loop
+for (i in 1:10) {
+    print(i)
 }
 
-
-##-----------------------------
-numbers*2 #element wise computation
-
-friends<- c("Toy", "Aan", "Wann")
-
-for (friend in friends){
-  print(paste("Hello! ", friend))
-}
-
-
-paste("Hello! ", friends)
-
-
-
-## 3. while loop
-while (TRUE){
-  #do someting
+scores <- c(95, 50, 88, 72, 75)
+for (score in scores) {
+    if (score >= 90) {
+        print("A")
+    } else if (score >= 80) {
+        print("B")
+    } else if (score >= 70) {
+        print("C")
+    } else{
+        print("D")
+    }
 }
 
 
-x <- 0
-
-while (x < 10 ){
-  print("hello!")
-  x <- x + 1
+## while loop
+count <- 0
+while (count < 10) {
+    print("hello")
+    count <- count + 1
 }
 
 
-
-## check username & password
-
-check_in <- function () {
-  user_input <- readline( "What is your username: ")
-  user_pw <- readline( "What is your password: ")
-  
-  sys_un <- "toy"
-  sys_pw <- "123456"
-
-  if(user_input == sys_un & user_pw == sys_pw ) {
-    print ("Log in Suscessfully")
-  } else {
-    print ("Please check your username & password")
-  }
+### Functions ####
+greeting <- function() {
+    print("Hello")
 }
 
-## ==================================
+my_sum <- function(a, b) {
+    # return(a + b)
+    a + b # R function's final line of code will automatically return
+}
 
-check_in <- function () {
-  user_input <- readline( "What is your username: ")
-  user_pw <- readline( "What is your password: ")
-  
-  sys_un <- c("toy","ann","wan")
-  sys_pw <- "123456"
-  
-  if(user_input %in% sys_un & user_pw == sys_pw ) {
-    print ("Log in Suscessfully")
-  } else {
-    print ("Please check your username & password")
-  }
+my_sum2 <- function(a, b)
+    a + b # {} can be omitted if function has only one statement
+
+
+## Control Flow in Function
+grading <- function(score) {
+    if (score >= 90) {
+        return("A")
+    } else if (score >= 80) {
+        return("B")
+    } else if (score >= 70) {
+        return("C")
+    } else{
+        return("D")
+    }
+}
+
+scores <- c(95, 50, 88, 72, 75)
+for (score in scores) {
+    print(grading(score))
 }
 
 
+# guess animal name ####
+guess_animal_game <- function(animal) {
+    while (TRUE) {
+        guess_animal <- readline("Guess animal: ")
+        
+        if (animal == guess_animal) {
+            print("Congratulation! your guess is correct!")
+            break
+        } else {
+            print("Your guess is incorrect. Please try again.")
+        }
+    }
+}
+
+guess_animal_game("hippo")
 
 
+# username passord login ####
+login_fn <- function() {
+    attempt <- 3
+    
+    username <- "user1234"
+    password <- "1234"
+    
+    count <- 1
+    while (count <= attempt) {
+        # get input from user
+        user_input <- readline("Username: ")
+        password_input <- readline("Password: ")
+        
+        # check username, password
+        if (username == user_input & password == password_input) {
+            print("Thank you! you have succesfully login.")
+            break
+            
+        } else {
+            print("Sorry, please tye again.")
+        }
+        count <- count + 1
+    }
+}
+
+login_fn()
 
 
+### Regular Expression ####
+# built-in data set state.name
 
+grep("^S", state.name) # grep return index
+state.name[grep("^S", state.name)]
+state.name[grep("^s", state.name, ignore.case = TRUE)]
+state.name[grep("^[ABS]",
+                state.name,
+                ignore.case = TRUE)] # return value by subset
 
+grep("^[abs]",
+     state.name,
+     ignore.case = TRUE,
+     value = TRUE) # return value
 
+grepl("^[ABS]", state.name) # grepl return logical
+x <- grepl("^[ABS]", state.name)
+sum(x)
 
+# replace
+gsub("South","Dragon", state.name)
